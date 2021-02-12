0f2eae02eefcac10bdcb62742dd8876c58317033
Evgeny Lyozin <evgenylyozindev@gmail.com>
2021-02-12 17:42:30 +0300

    ch19

---
+++ ch19/src/main/java/com/packtpub/reflection/DependencyInjector.java
@@ -0,0 +1,36 @@
+package com.packtpub.reflection;
+
+import java.lang.reflect.Field;
+
+public class DependencyInjector {
+    void setField(Object object, Class c, String fieldName, Object value) {
+        try {
+            Field field = c.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(object, value);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Database init(String username, String password,
+                         String url, String driver) {
+        Database database = new Database();
+        Class c = Database.class;
+        // Set values.
+        setField(database,c,"username",username);
+        setField(database,c,"password",password);
+        setField(database,c,"url",url);
+        setField(database,c,"driver",driver);
+        return database;
+    }
+
+    public static void main(String[] args) {
+        DependencyInjector injector = new DependencyInjector();
+        Database database = injector.init("bobmarley", "password",
+                "jdbc:h2:~/test", "org.h2.Driver");
+        System.out.println(database.toString());
+    }
+}
